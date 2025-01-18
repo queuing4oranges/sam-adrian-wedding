0b961030accba6d193d11bc115e1da60fb859cc8
queuing4oranges <katjazenker@gmail.com>
2025-01-18 22:22:47 +0100

    Re-structure information/ itinerary data

--- src/components/itinerary/itineraryData.js
+++ src/components/itinerary/itineraryData.js
@@ -2,36 +2,36 @@ export const itineraryData = [
 	{
 		day: '1',
 		date: 'Monday, Sept 8',
-		note: 'Mandatory for friends flying in from the US',
-		title: 'Welcome and explore Prague with a day tour hosted by Adrian and Sam. Let&apos;s also enjoy a relaxed evening hangout with typical Czech food and for those interested tasty beer.',
-		description: 'We will spend the day exploring old town, Prague highlights, and our favorite hidden gems.'
+		title: 'Welcome to Prague.',
+		description: 'We will spend the day exploring old town, Prague highlights, and our favorite hidden gems.',
+		note: 'Mandatory for friends flying in from the US. Explore Prague with a day tour hosted by Adrian and Sam. Let&apos;s also enjoy a relaxed evening hangout with typical Czech food and for those interested tasty beer.'
 	},
 	{
 		day: '2',
 		date: 'Tuesday, Sept 9',
-		note: 'You can sign up to volunteer your car via the RSVP form. We&apos;re leaving Prague around 11 a.m. and expect to arrive in the cottage at around 2 p.m. The train takes about 3 hours, by car around 2 hours. Some of us WILL need to take the train. Sam will lead the train ride, which should be both fun and relaxing.',
 		title: 'Travel to the woods (train or carpool) for games and fun in a cottage retreat.',
-		description: 'Expect games and a casual dinner at the cottage.'
+		description: 'Expect games and a casual dinner at the cottage. We&apos;re leaving Prague around 11 a.m. and expect to arrive in the cottage at around 2 p.m. The train takes about 3 hours, by car around 2 hours. Some of us WILL need to take the train. Sam will lead the train ride, which should be both fun and relaxing. ',
+		note: 'You can sign up to volunteer your car via the RSVP form.',
 	},
 	{
 		day: '3',
 		date: 'Wednesday, Sept 10',
-		note: 'Something something something and then also something and maybe look at the pictures of what to wear and please don&apos;t wear what you are not supposed to wear. Naked is good too - if you are a woman. This text has not been written by a gay woman!',
 		title: 'Formal day: Dress elegantly and party in style.',
-		description: 'Start will be around 4 p.m. Please look at the photos for inspiration.'
+		description: 'Something something something and then also something and maybe look at the pictures of what to wear and please don&apos;t wear what you are not supposed to wear. Naked is good too - if you are a woman. This text has not been written by a gay woman!',
+		note: 'Start will be around 4 p.m. Please look at the photos for inspiration.'
 	},
 	{
 		day: '4',
 		date: 'Thursday, Sept 11',
-		note: 'Mandatory to dress up. There will be a competition.',
 		title: 'Themed party: Show off your best Shrek-inspired outfits!',
-		description: 'Look at the photos for inspiration and at the list of the characters.'
+		description: 'Look at the photos for inspiration and at the list of the characters.',
+		note: 'Mandatory to dress up. There will be a competition.'
 	},
 	{
 		day: '5',
 		date:'Friday, Sept 12',
-		note: 'Anyone still in town and interested can continue to hang with us, we can continue to explore Prague as there is much to see and do!',
 		title: 'Departure back to Prague.',
-		description: 'We are going to check out at 11 a.m. and are heading back to Prague by car or train.'
+		description: 'We are going to check out at 11 a.m. and are heading back to Prague by car or train.',
+		note: 'Anyone still in town and interested can continue to hang with us, we can continue to explore Prague as there is much to see and do!'
 	}
 ]
