5928abd04c0073ed330206a7e8ead166601d15e2
queuing4oranges <katjazenker@gmail.com>
2025-01-19 19:38:05 +0100

    Update itinerary data

--- src/components/itinerary/itineraryData.js
+++ src/components/itinerary/itineraryData.js
@@ -3,35 +3,39 @@ export const itineraryData = [
 		day: '1',
 		date: 'Monday, Sept 8',
 		title: 'Welcome to Prague.',
-		description: 'We will spend the day exploring old town, Prague highlights, and our favorite hidden gems.',
-		note: 'Mandatory for friends flying in from the US. Explore Prague with a day tour hosted by Adrian and Sam. Let&apos;s also enjoy a relaxed evening hangout with typical Czech food and for those interested tasty beer.'
+		description: 'We will spend the day exploring Old Town, discovering Prague highlights, and uncovering our favorite hidden gems.',
+		note: 'This is mandatory for our friends flying in from the US! Join Adrian and Sam for a day tour of Prague, where we&apos;ll explore the city together. We&apos;ll also enjoy a relaxed evening with typical Czech food, and for those interested, some delicious local beer.',
+		className: 'prague',
 	},
 	{
 		day: '2',
 		date: 'Tuesday, Sept 9',
 		title: 'Travel to the woods (train or carpool) for games and fun in a cottage retreat.',
-		description: 'Expect games and a casual dinner at the cottage. We&apos;re leaving Prague around 11 a.m. and expect to arrive in the cottage at around 2 p.m. The train takes about 3 hours, by car around 2 hours. Some of us WILL need to take the train. Sam will lead the train ride, which should be both fun and relaxing. ',
+		description: 'We&apos;ll be leaving Prague around 11 a.m. and expect to arrive at the cottage around 2 p.m. The train ride takes about 1.5 hours, while the drive by car is about 1 hour. Some of us will need to take the train, and Sam will be leading the ride, which promises to be both fun and relaxing. Expect games and a casual dinner at the cottage! ',
 		note: 'You can sign up to volunteer your car via the RSVP form.',
+		className: 'forrest'
 	},
 	{
 		day: '3',
 		date: 'Wednesday, Sept 10',
 		title: 'Formal day: Dress elegantly and party in style.',
-		description: 'Something something something and then also something and maybe look at the pictures of what to wear and please don&apos;t wear what you are not supposed to wear. Naked is good too - if you are a woman. This text has not been written by a gay woman!',
-		note: 'Start will be around 4 p.m. Please look at the photos for inspiration.'
+		description: 'We can&apos;t wait to celebrate with you! Expect an evening with great music, dancing, and plenty of laughter. Dress code is Cocktail or Creative Black Tie. The current start time is set for 4 p.m., but if anything changes, we will be sure to keep you updated.',
+		note: 'We have photos available for reference, but they&apos;re just examples—feel free to be comfortable and have fun putting together stylish outfits that reflect your unique flair. We know you&apos;ll look fantastic!',
+		className: 'party'
 	},
 	{
 		day: '4',
 		date: 'Thursday, Sept 11',
-		title: 'Themed party: Show off your best Shrek-inspired outfits!',
-		description: 'Look at the photos for inspiration and at the list of the characters.',
-		note: 'Mandatory to dress up. There will be a competition.'
+		title: 'Themed party: Go hard or go home with your best Shrek-inspired outfits!',
+		description:'For our Shrek costume party, you&apos;re welcome to dress up as any character you love—none are off-limits! Adrian has chosen to go as Lord Farquaad, and Sam will be Princess Fiona. Don&apos;t worry about duplicates—we&apos;re more than happy to have multiple versions of the same costume. Feel free to pick any character that inspires you! We&apos;re so excited to see everyone&apos;s creative interpretations. Here&apos;s a reminder of some fun options: Donkey, Puss in Boots, Dragon, Gingy, the Three Blind Mice, the Three Little Pigs, Big Bad Wolf, Muffin Man, Magic Mirror, and so many more!',
+		note: 'Dressing up is mandatory! You can reference the photos for inspiration, and there will even be a competition.'
 	},
 	{
 		day: '5',
 		date:'Friday, Sept 12',
 		title: 'Departure back to Prague.',
 		description: 'We are going to check out at 11 a.m. and are heading back to Prague by car or train.',
-		note: 'Anyone still in town and interested can continue to hang with us, we can continue to explore Prague as there is much to see and do!'
+		note: 'Anyone still in town and interested can continue to hang with us, we can continue to explore Prague as there is much to see and do!',
+		className: 'train'
 	}
 ]
