3ff59bbf3d5f2877b093ec5a55dec3b4da8f0f8d
queuing4oranges <katjazenker@gmail.com>
2025-01-26 13:44:53 +0100

    Add icon, update data

---
+++ src/assets/svgs/Xicon.jsx
@@ -0,0 +1,13 @@
+export default function Xicon({ width, height, color }) {
+  return (
+	<svg width={width} height={height} viewBox="0 -0.5 8 8">
+		<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+			<g id="Dribbble-Light-Preview" transform="translate(-385.000000, -206.000000)" fill={color}>
+				<g id="icons" transform="translate(56.000000, 160.000000)">
+					<polygon id="close_mini-[#1522]" points="334.6 49.5 337 51.6 335.4 53 333 50.9 330.6 53 329 51.6 331.4 49.5 329 47.4 330.6 46 333 48.1 335.4 46 337 47.4"></polygon>
+				</g>
+			</g>
+		</g>
+	</svg>
+  );
+}
--- src/components/itinerary/itineraryData.js
+++ src/components/itinerary/itineraryData.js
@@ -1,41 +1,48 @@
+import prague from "../../assets/images/prague01.png";
+import forrest from "../../assets/images/forrest.png";
+import costume from "../../assets/images/costume.png";
+import party from "../../assets/images/party.png";
+import train from "../../assets/images/train.png";
+
 export const itineraryData = [
 	{
-		day: '1',
-		date: 'Monday, Sept 8',
-		title: 'Welcome to Prague.',
-		description: 'We will spend the day exploring Old Town, discovering Prague highlights, and uncovering our favorite hidden gems.',
-		note: 'This is mandatory for our friends flying in from the US! Join Adrian and Sam for a day tour of Prague, where we&apos;ll explore the city together. We&apos;ll also enjoy a relaxed evening with typical Czech food, and for those interested, some delicious local beer.',
-		className: 'prague',
+		day: "1",
+		date: "Monday, Sept 8",
+		title: "Welcome to Prague.",
+		description: "We will spend the day exploring Old Town, discovering Prague highlights, and uncovering our favorite hidden gems.",
+		note: "This is mandatory for our friends flying in from the US! Join Adrian and Sam for a day tour of Prague, where we'll explore the city together. We'll also enjoy a relaxed evening with typical Czech food, and for those interested, some delicious local beer.",
+		imageUrl: prague
 	},
 	{
-		day: '2',
-		date: 'Tuesday, Sept 9',
-		title: 'Travel to the woods (train or carpool) for games and fun in a cottage retreat.',
-		description: 'We&apos;ll be leaving Prague around 11 a.m. and expect to arrive at the cottage around 2 p.m. The train ride takes about 1.5 hours, while the drive by car is about 1 hour. Some of us will need to take the train, and Sam will be leading the ride, which promises to be both fun and relaxing. Expect games and a casual dinner at the cottage! ',
-		note: 'You can sign up to volunteer your car via the RSVP form.',
-		className: 'forrest'
+		day: "2",
+		date: "Tuesday, Sept 9",
+		title: "Travel to the woods (train or carpool) for games and fun in a cottage retreat.",
+		description: "We'll be leaving Prague around 11 a.m. and expect to arrive at the cottage around 2 p.m. The train ride takes about 1.5 hours, while the drive by car is about 1 hour. Some of us will need to take the train, and Sam will be leading the ride, which promises to be both fun and relaxing. Expect games and a casual dinner at the cottage! ",
+		note: "You can sign up to volunteer your car via the RSVP form.",
+		imageUrl: forrest
 	},
 	{
-		day: '3',
-		date: 'Wednesday, Sept 10',
-		title: 'Formal day: Dress elegantly and party in style.',
-		description: 'We can&apos;t wait to celebrate with you! Expect an evening with great music, dancing, and plenty of laughter. Dress code is Cocktail or Creative Black Tie. The current start time is set for 4 p.m., but if anything changes, we will be sure to keep you updated.',
-		note: 'We have photos available for reference, but they&apos;re just examples—feel free to be comfortable and have fun putting together stylish outfits that reflect your unique flair. We know you&apos;ll look fantastic!',
-		className: 'party'
+		day: "3",
+		date: "Wednesday, Sept 10",
+		title: "Formal day: Dress elegantly and party in style.",
+		description: "We can't wait to celebrate with you! Expect an evening with great music, dancing, and plenty of laughter. Dress code is Cocktail or Creative Black Tie. The current start time is set for 4 p.m., but if anything changes, we will be sure to keep you updated.",
+		note: "We have photos available for reference, but they're just examples—feel free to be comfortable and have fun putting together stylish outfits that reflect your unique flair. We know you'll look fantastic!",
+		imageUrl: party
 	},
 	{
-		day: '4',
-		date: 'Thursday, Sept 11',
-		title: 'Themed party: Go hard or go home with your best Shrek-inspired outfits!',
-		description:'For our Shrek costume party, you&apos;re welcome to dress up as any character you love—none are off-limits! Adrian has chosen to go as Lord Farquaad, and Sam will be Princess Fiona. Don&apos;t worry about duplicates—we&apos;re more than happy to have multiple versions of the same costume. Feel free to pick any character that inspires you! We&apos;re so excited to see everyone&apos;s creative interpretations. Here&apos;s a reminder of some fun options: Donkey, Puss in Boots, Dragon, Gingy, the Three Blind Mice, the Three Little Pigs, Big Bad Wolf, Muffin Man, Magic Mirror, and so many more!',
-		note: 'Dressing up is mandatory! You can reference the photos for inspiration, and there will even be a competition.'
+		day: "4",
+		date: "Thursday, Sept 11",
+		title: "Themed party: Go hard or go home with your best Shrek-inspired outfits!",
+		description:"For our Shrek costume party, you're welcome to dress up as any character you love—none are off-limits! Adrian has chosen to go as Lord Farquaad, and Sam will be Princess Fiona. Don't worry about duplicates—we're more than happy to have multiple versions of the same costume. Feel free to pick any character that inspires you! We're so excited to see everyone's creative interpretations. Here's a reminder of some fun options: Donkey, Puss in Boots, Dragon, Gingy, the Three Blind Mice, the Three Little Pigs, Big Bad Wolf, Muffin Man, Magic Mirror, and so many more!",
+		note: "Dressing up is mandatory! You can reference the photos for inspiration, and there will even be a competition.",
+		imageUrl: costume
 	},
 	{
-		day: '5',
-		date:'Friday, Sept 12',
-		title: 'Departure back to Prague.',
-		description: 'We are going to check out at 11 a.m. and are heading back to Prague by car or train.',
-		note: 'Anyone still in town and interested can continue to hang with us, we can continue to explore Prague as there is much to see and do!',
-		className: 'train'
+		day: "5",
+		date:"Friday, Sept 12",
+		title: "Departure back to Prague.",
+		description: "We are going to check out at 11 a.m. and are heading back to Prague by car or train.",
+		note: "Anyone still in town and interested can continue to hang with us, we can continue to explore Prague as there is much to see and do!",
+		imageUrl: train
 	}
 ]
