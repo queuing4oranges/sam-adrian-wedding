1d2e8cadee55540796475adf3e209b5853c267f2
queuing4oranges <katjazenker@gmail.com>
2025-02-02 20:36:06 +0100

    Remove background

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -1,5 +1,7 @@
-import { Container, Row, Col } from 'reactstrap';
-import Akkordeon from './Akkordeon'
+import { Container, Row, Col, Card } from 'reactstrap';
+
+import Akkordeon from './Akkordeon';
+import LayingDoodle from '../../assets/svgs/LayingDoodle';
 
 import './faq.scss';
 
@@ -8,15 +10,27 @@ export default function faq() {
 		<Container
 			fluid
 			id='faq'
-			className='faq-container w-100 h-100 pb-5 px-5'
+			className='faq-container w-100 p-5'
 		>
 			<Row className='pt-5'>
-				<h1 className='pt-5 px-5 text-center'>FAQ</h1>
+				<h1 className='px-5 text-center'>FAQ</h1>
 			</Row>
-			<Row className='pt-5 w-50 mx-auto'>
+			<Row className='py-5 w-50 mx-auto'>
 				<Col>
 					<Akkordeon />
 				</Col>
+			</Row>
+			<Row className='py-5'>
+				<h1 className='px-5 text-center'>RSVP</h1>
+			</Row>
+			<Row>
+				<Card>
+					<span style={{height: 'auto', width: '30rem'}}><LayingDoodle /></span>
+					
+					{/* rsvp button - link */}
+					{/* email btn */}
+					{/* add event to calendar */}
+				</Card>
 			</Row>
 		</Container>
 	)
