f7ec9b73916421a58b81aff828d1885c70ac3b82
queuing4oranges <katjazenker@gmail.com>
2025-02-07 21:50:12 +0100

    Refactor accordion, refactor RSVP btn

--- src/components/faq/Akkordeon.jsx
+++ src/components/faq/Akkordeon.jsx
@@ -1,64 +1,55 @@
-import { useState } from 'react';
-import {
-	Accordion,
-	AccordionBody,
-	AccordionHeader,
-	AccordionItem,
-	Button
-} from 'reactstrap';
+import './akkordeon.scss';
 
-function Example() {
-	const [open, setOpen] = useState('');
+export default function Akkordeon() {
+	return (
+		<nav className='accordion arrows'>
 
-	const toggle = (id) => {
-		if (open === id) {
-			setOpen();
-		} else {
-			setOpen(id);
-		}
-	};
+			<input type='radio' name='accordion' id='cb1' />
+			<section className='box'>
+				<label className='box-title' htmlFor='cb1'>What should I pack?</label>
+				<label className='box-close' htmlFor='acc-close'></label>
+				<div className='box-content'>
+					Include a swimsuit! <br />
+					We won&apos;t know if there will be rain until the date is closer and I will let everyone know, regardless we will have fun
+				</div>
+			</section>
 
-return (
-	<Accordion open={open} toggle={toggle}>
-		<AccordionItem>
-			<AccordionHeader targetId='1'><h3>What should I pack?</h3></AccordionHeader>
-			<AccordionBody accordionId='1'>
-				<ul>
-					<li>Include a swimsuit!</li>
-					<li>We won&apos;t know if there will be rain until the date is closer and I will let everyone know, regardless we will have fun</li>
-				</ul>
-			</AccordionBody>
-		</AccordionItem>
-		<AccordionItem>
-			<AccordionHeader targetId='2'><h3>How do I sign up for carpooling?</h3></AccordionHeader>
-			<AccordionBody accordionId='2'>
-				<ul>
-					<li>Use our RSVP Form</li>
-				</ul>
-				<span>
-					<i className='bi bi-heart-arrow ms-5 me-2'/>
-					<Button>RSVP</Button>
-				</span>
-			</AccordionBody>
-		</AccordionItem>
-		<AccordionItem>
-			<AccordionHeader targetId='3'><h3>Can I bring a plus one?</h3></AccordionHeader>
-			<AccordionBody accordionId='3'>
-				<ul>
-					<li>No, all guests personally invited are welcomed only.</li>
-				</ul>
-			</AccordionBody>
-		</AccordionItem>
-				<AccordionItem>
-			<AccordionHeader targetId='4'><h3>How will our time be spent aside from drinking/partying in the evenings?</h3></AccordionHeader>
-			<AccordionBody accordionId='4'>
-				<ul>
-					<li>During the day we will have time to play volleyball, swim, walk in nature, play games, or just relax. Activities may depend on the weather and your preferences. There will be free unscheduled time too.</li>
-				</ul>
-			</AccordionBody>
-		</AccordionItem>
-	</Accordion>
-);
+			<input type='radio' name='accordion' id='cb2' />
+			<section className='box'>
+				<label className='box-title' htmlFor='cb2'>How do I sign up for carpooling?</label>
+				<label className='box-close' htmlFor='acc-close'></label>
+				<div className='box-content'>
+					<span className='d-flex align-items-center'>
+						<p className='m-0 me-2'>
+							Use our RSVP Form
+							<i className='bi bi-arrow-right-short' />
+						</p>
+						<button className='button-55' role='button'>
+							<a href='https://docs.google.com/forms/d/e/1FAIpQLSeb96PN2yCs9EsAPzcfAT0wC8Y9TrquhyJWD7JDcFU-VWp8bw/viewform' target='_blank' className='text-decoration-none text-dark'>
+								RSVP
+							</a>
+						</button>
+					</span>
+				</div>
+			</section>
+
+			<input type='radio' name='accordion' id='cb3' />
+			<section className='box'>
+				<label className='box-title' htmlFor='cb3'>Can I bring a plus one?</label>
+				<label className='box-close' htmlFor='acc-close'></label>
+				<div className='box-content'>
+					No, all guests personally invited are welcomed only.
+				</div>
+			</section>
+
+			<input type='radio' name='accordion' id='cb4' />
+			<section className='box'>
+				<label className='box-title' htmlFor='cb4'>How will our time be spent aside from drinking/partying in the evenings?</label>
+				<label className='box-close' htmlFor='acc-close'></label>
+				<div className='box-content'>
+					During the day we will have time to play volleyball, swim, walk in nature, play games, or just relax. Activities may depend on the weather and your preferences. There will be free unscheduled time too.
+				</div>
+			</section>
+		</nav>
+	);
 }
-
-export default Example;
--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -6,6 +6,56 @@ import LayingDoodle from '../../assets/svgs/LayingDoodle';
 import './faq.scss';
 
 export default function faq() {
+	const userAgent = navigator.userAgent.toLowerCase(); // Check which device user is using
+
+	const handleAddToCalendar = () => {
+		if (/iphone|ipad|mac os/.test(userAgent)) {
+			handleAppleCalendar();  // iOS or Mac users
+		} else {
+			handleGoogleCalendar(); // Default to Google Calendar
+		}
+	};
+
+	// Create a calender event for Google user
+	const handleGoogleCalendar = () => {
+		// Construct URL
+		const googleCalendarUrl = `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent("Sam and Adrian's Wedding Celebration")}&dates=${'20250908T120000'}/${'20250912T120000'}&location=${encodeURIComponent('Prague')}&sprop=&sprop=name:`;
+
+		window.open(googleCalendarUrl, '_blank');
+	};
+
+	// Create a calender event for Apple user
+	const handleAppleCalendar = () => {
+		// Strip leading whitespace
+		const stripIndent = (str) => str.replace(/^\s+/gm, ''); // Preventing problems with whitespace in isContent
+
+		// Create the .ics file content
+		const icsContent = stripIndent(`
+			BEGIN:VCALENDAR
+			VERSION:2.0
+			BEGIN:VEVENT
+			SUMMARY:'Sam and Adrian's Wedding Celebration'
+			DTSTART:'20250908T120000'
+			DTEND:'20250912T120000'
+			LOCATION:'Prague'
+			END:VEVENT
+			END:VCALENDAR
+		`);
+
+		// Create a Blob with the .ics content
+		const blob = new Blob([icsContent], { type: 'text/calendar;charset=utf-8' });
+		const url = window.URL.createObjectURL(blob);
+		const link = document.createElement('a');
+		link.href = url;
+		link.setAttribute('download', 'wedding.ics');
+		document.body.appendChild(link);
+
+		// Trigger a click on the link to start the download
+		link.click();
+
+		// Remove the link from the document
+		document.body.removeChild(link);
+	};
 
 	return (
 		<Container
@@ -47,25 +97,29 @@ export default function faq() {
 						</Col>
 						<Col md='4' className='p-5'>
 							<p>
-								Please RSVP by August, 10 to help us finalize arrangements. <br /> Incase you missed it above here, again, the RSVP form:
+								Please RSVP by <strong>August, 10 </strong>to help us finalize arrangements. <br /> Incase you missed it above here, again, the RSVP form:
 							</p>
 						</Col>
 						<Col md='4' className='p-5 text-end'>
 							<p>
-								Join us in celebrating! Save the date to your calendar.
+								Join us in celebrating! <br />
+								Save the date to your calendar.
 							</p>
-							<div className="btn-wrapper d-flex justify-content-end">
+							<div
+								className="btn-wrapper d-flex justify-content-end"
+								onClick={() => handleAddToCalendar()}
+							>
 								<span className='span-container'>
 									<span>
-										<i className='pe-2 bi bi-envelope-heart' />
-										Email us!
+										<i className='pe-2 bi bi-calendar-heart' />
+										Add to calendar
 									</span>
 								</span>
 							</div>
 						</Col>
 					</Row>
 					<div className='d-flex justify-content-center pb-5'>
-						<div className='d-flex align-items-center gap-1 chevron-container'>
+						<div className='d-flex align-items-center'>
 							<button className='rsvp-btn'>
 								<a href='https://docs.google.com/forms/d/e/1FAIpQLSeb96PN2yCs9EsAPzcfAT0wC8Y9TrquhyJWD7JDcFU-VWp8bw/viewform' target='_blank' className='text-decoration-none'>
 									<i className='bi bi-chevron-double-right me-2' />
