aadec43c2a53d826a468c7f78dcdadaed0322e1e
queuing4oranges <katjazenker@gmail.com>
2025-02-08 12:41:16 +0100

    Make welcome section responsive

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -6,26 +6,30 @@ import '../../components/button55.scss';
 export default function Welcome() {
 
 	return (
-		<Container fluid id='welcome' className='welcome-container w-100 h-100 px-0 position-relative'>
-			<Row className='pt-5 w-75 mx-auto'>
-				<span className='sam-adrian text-center'>Sam & Adrian</span>
-			</Row>
-			<Row className='w-100 d-flex' style={{ margin: 'auto'}}>
-				<Col className='mx-auto p-4'>
-					<h1 className='text-center'>Join Us for an Unforgettable Wedding Celebration in Prague!</h1>
-					<br />
-					<h2 className='text-center'>You are formally invited to join us in a celebration of love, laughter, and adventure.</h2>
+		<Container fluid id='welcome' className='welcome-container w-100 h-100 px-0 position-relative d-flex flex-column align-items-center justify-content-center'>
+			<Row className='pt-5 w-100 d-flex justify-content-center'>
+				<Col xs={12} md={8} className="text-center">
+					<span className="sam-adrian fw-bold">Sam & Adrian</span>
 				</Col>
 			</Row>
-			<Row className='w-50 mx-auto'>
-				<span className='text-center mt-5'>
-					<button className='button-55 welcome-rsvp-btn' role='button' title='Let&apos;s go!'>
-						<a href='https://docs.google.com/forms/d/e/1FAIpQLSeb96PN2yCs9EsAPzcfAT0wC8Y9TrquhyJWD7JDcFU-VWp8bw/viewform' target='_blank' className='text-decoration-none text-dark'>
+			<Row className='w-100 d-flex justify-content-center'>
+				<Col xs={12} md={10} lg={8} className="mx-auto p-4">
+					<h1 className='text-center fs-1 fw-bold'>Join Us for an Unforgettable Wedding Celebration in Prague!</h1>
+					<br />
+					<h2 className='text-center fs-3 fw-bold mt-3'>You are formally invited to join us in a celebration of love, laughter, and adventure.</h2>
+				</Col>
+			</Row>
+			<Row className='d-flex justify-content-center'>
+				<Col xs={12} md={12} className="text-center">
+					<a href='https://docs.google.com/forms/d/e/1FAIpQLSeb96PN2yCs9EsAPzcfAT0wC8Y9TrquhyJWD7JDcFU-VWp8bw/viewform' target='_blank' className='text-decoration-none text-dark'>
+						<button className='button-55 welcome-rsvp-btn' role='button' title='Let&apos;s go!'>
 							RSVP
-						</a>
-					</button>
-				</span>
+						</button>
+					</a>
+				</Col>
 			</Row>
 		</Container>
 	)
 }
+
+//TODO refactor double to single quotes here
