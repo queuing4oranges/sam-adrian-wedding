978394f09def2bffde6fcda9e92e7f254b97a91d
queuing4oranges <katjazenker@gmail.com>
2025-02-08 12:42:51 +0100

    Refactor double to single quotes

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -8,19 +8,19 @@ export default function Welcome() {
 	return (
 		<Container fluid id='welcome' className='welcome-container w-100 h-100 px-0 position-relative d-flex flex-column align-items-center justify-content-center'>
 			<Row className='pt-5 w-100 d-flex justify-content-center'>
-				<Col xs={12} md={8} className="text-center">
-					<span className="sam-adrian fw-bold">Sam & Adrian</span>
+				<Col xs={12} md={8} className='text-center'>
+					<span className='sam-adrian fw-bold'>Sam & Adrian</span>
 				</Col>
 			</Row>
 			<Row className='w-100 d-flex justify-content-center'>
-				<Col xs={12} md={10} lg={8} className="mx-auto p-4">
+				<Col xs={12} md={10} lg={8} className='mx-auto p-4'>
 					<h1 className='text-center fs-1 fw-bold'>Join Us for an Unforgettable Wedding Celebration in Prague!</h1>
 					<br />
 					<h2 className='text-center fs-3 fw-bold mt-3'>You are formally invited to join us in a celebration of love, laughter, and adventure.</h2>
 				</Col>
 			</Row>
 			<Row className='d-flex justify-content-center'>
-				<Col xs={12} md={12} className="text-center">
+				<Col xs={12} md={12} className='text-center'>
 					<a href='https://docs.google.com/forms/d/e/1FAIpQLSeb96PN2yCs9EsAPzcfAT0wC8Y9TrquhyJWD7JDcFU-VWp8bw/viewform' target='_blank' className='text-decoration-none text-dark'>
 						<button className='button-55 welcome-rsvp-btn' role='button' title='Let&apos;s go!'>
 							RSVP
@@ -31,5 +31,3 @@ export default function Welcome() {
 		</Container>
 	)
 }
-
-//TODO refactor double to single quotes here
