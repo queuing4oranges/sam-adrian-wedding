9f5ff311ba9635895a94346bc5c4c0f69cce6d78
queuing4oranges <katjazenker@gmail.com>
2025-02-16 16:29:56 +0100

    Fix x button glitch

--- src/components/itinerary/SingleDayInfo.jsx
+++ src/components/itinerary/SingleDayInfo.jsx
@@ -17,42 +17,41 @@ export default function SingleDayInfo({ setShowMore, schedule, selectedDay }) {
 	return (
 		<Row className='w-100 w-lg-50 card-container'>
 			<Col xs={12} lg={6} className='d-flex flex-column justify-content-center'>
-				<span
-					onClick={() => setShowMore(false)}
-					className='close-x d-flex justify-content-end pb-3'
-					title='Close'
-				>
-					<Xicon
-						width={30}
-						height={30}
-						color='#164443'
-						className='xicon'
-					/>
-				</span>
-				{!modal ?
-				<Card>
-					<div
-						className='show-bg'
-						style={{ backgroundImage: `url(${imageUrl})`}}>
-					</div>
-					<CardBody>
-						<h3 className='mb-5'>Day {day} - {date}</h3>
-						<h1 className='mb-5'>{title}</h1>
-						<p className='fw-bold mb-3'>{description}</p>
-						<p>{note}</p>
-						{inspirationUrl &&
-							<Button color='info' onClick={toggle} className='d-flex'>
-								<i className='bi bi-card-image me-2' />
-								<p className='m-0'>Photos for inspiration</p>
-							</Button>
-						}
-					</CardBody>
-				</Card>
+				{!modal ? <>
+					<span
+						onClick={() => setShowMore(false)}
+						className='close-x d-flex justify-content-end pb-3'
+						title='Close'
+					>
+						<Xicon
+							width={30}
+							height={30}
+							color='#164443'
+							className='xicon'
+						/>
+					</span>
+					<Card>
+						<div
+							className='show-bg'
+							style={{ backgroundImage: `url(${imageUrl})`}}>
+						</div>
+						<CardBody>
+							<h3 className='mb-5'>Day {day} - {date}</h3>
+							<h1 className='mb-5'>{title}</h1>
+							<p className='fw-bold mb-3'>{description}</p>
+							<p>{note}</p>
+							{inspirationUrl &&
+								<Button color='info' onClick={toggle} className='d-flex'>
+									<i className='bi bi-card-image me-2' />
+									<p className='m-0'>Photos for inspiration</p>
+								</Button>
+							}
+						</CardBody>
+					</Card>
+				</>
 				: <FotoModal inspirationUrl={inspirationUrl} toggle={toggle} modal={modal} />
 				}
 			</Col>
-
-
 		</Row>
 	)
 }
@@ -62,7 +61,7 @@ function FotoModal ({ inspirationUrl, toggle, modal }) {
 		<Modal
 			isOpen={modal}
 			toggle={toggle}
-			size='sm'
+			size='lg'
 			scrollable={true}
 			className='d-flex justify-content-center'
 		>
@@ -73,7 +72,7 @@ function FotoModal ({ inspirationUrl, toggle, modal }) {
 							key={idx}
 							src={image}
 							alt={`Inspiration photo ${idx +1}`}
-							className='m-3'
+							className='img-fluid my-3'
 						/>
 					))}
 				</div>
